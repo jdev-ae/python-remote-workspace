1643c2ef727889c33ec81c056db58c749e8e5e9f
SuRu <su33.dev@gmail.com>
2019-06-26 23:26:32 +0300

    added user auth related code

--- projects/flask_blog/flaskblog/__init__.py
+++ projects/flask_blog/flaskblog/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,5 +9,10 @@ app.config['SECRET_KEY'] = '6d0e21d41cbc2309e3656ab929ad21f2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dev_db.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- projects/flask_blog/flaskblog/routes.py
+++ projects/flask_blog/flaskblog/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
+
+from flaskblog import app, bcrypt, db
 from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog import app
+from flaskblog.models import User
 
 test_posts = [
     {
@@ -20,6 +23,7 @@ test_posts = [
 
 @app.route("/")
 @app.route("/home")
+@login_required
 def home():
     return render_template('home.html', posts=test_posts, title='My Posts')
 
@@ -36,21 +40,45 @@ def health():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login failed', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
