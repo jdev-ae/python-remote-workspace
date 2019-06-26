3e4ae58eb9a9802e786a12bbaace89c817b49759
SuRu <su33.dev@gmail.com>
2019-06-26 15:21:51 +0300

    Added forms and validations

--- projects/flask_blog/flaskblog.py
+++ projects/flask_blog/flaskblog.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 
 import test_data
+from blog_forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6d0e21d41cbc2309e3656ab929ad21f2'
 
 
 @app.route("/")
@@ -19,6 +22,28 @@ def about():
 @app.route("/health")
 def health():
     return "Working fine"
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
